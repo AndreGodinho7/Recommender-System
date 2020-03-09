82d4cd0b4040706ce57844b29867a6a5887afa50
quickacid <pedrobarbosa262@gmail.com>
2020-03-09 18:49:45 +0000

    Created a new struct _non_zero and added a new attribute to _input_values

--- Serial version/input.h
+++ Serial version/input.h
@@ -1,5 +1,14 @@
+
 #ifndef input
 #define input
+
+
+typedef struct _non_zero{
+  int row;
+  int column;
+  double value;
+}_non_zero;
+
 
 struct _input_values{
   int iter;
@@ -9,7 +18,9 @@ struct _input_values{
   int nI;
   int non_zeros;
   double** matrix;
+  _non_zero *v;
 };
+
 
 typedef struct _input_values input_values;
 
